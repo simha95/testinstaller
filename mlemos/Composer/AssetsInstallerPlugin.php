7a80a014f69460b338e3302d98521b0ea866ea11
simha95 <simhachalam.gulla@gmail.com>
2016-10-06 16:58:51 +0530

    test commit

--- mlemos/Composer/AssetsInstallerPlugin.php
+++ mlemos/Composer/AssetsInstallerPlugin.php
@@ -44,53 +44,7 @@ class AssetsInstallerPlugin implements PluginInterface, EventSubscriberInterface
 
 	public function onPreFileDownload(PreFileDownloadEvent $event)
 	{
-		if(!IsSet($this->authentication))
-		{
-			$this->authentication = array();
-			$fileName = 'auth.json';
-			if(file_exists($fileName))
-			{
-				if($this->io->isDebug())
-					$this->io->write('Reading autentication configuration from '.$fileName);
-				$json = file_get_contents($fileName);
-				$auth = json_decode($json);
-				if(GetType($auth) === 'object'
-				&& IsSet($auth->config)
-				&& GetType($auth->config) === 'object'
-				&& IsSet($auth->config->{'basic-auth'})
-				&& GetType($auth->config->{'basic-auth'}) === 'object')
-				{
-					foreach($auth->config->{'basic-auth'} as $url => $credentials)
-					{
-						$host = parse_url($url, PHP_URL_HOST);
-						if(!IsSet($host))
-							$host = $url;
-						if(!IsSet($credentials->username))
-						{
-							if($this->io->isDebug())
-								$this->io->write('The autentication credentials for '.$host.' are missing the username');
-						}
-						elseif(!IsSet($credentials->password))
-						{
-							if($this->io->isDebug())
-								$this->io->write('The autentication credentials for '.$host.' are missing the password');
-						}
-						else
-						{
-							$this->authentication[$host] = array(
-								'username'=>$credentials->username,
-								'password'=>$credentials->password
-							);
-						}
-					}
-				}
-				elseif($this->io->isDebug())
-					$this->io->write('The autentication definitions in the configuration file '.$fileName.' are not valid');
-			}
-		}
-		$host = parse_url($event->getProcessedUrl(), PHP_URL_HOST);
-		if(IsSet($this->authentication[$host]))
-			$this->io->setAuthentication($host, $this->authentication[$host]['username'], $this->authentication[$host]['password']);
+		
 	}
 };
 
